f0b44deeae6c9655426bbabd0ec3564b09055a25
Boh0263 <86723294+Boh0263@users.noreply.github.com>
2024-06-02 07:42:43 +0200

    id-6 resolved

--- src/main/java/control/SecurityHeadersFilter.java
+++ src/main/java/control/SecurityHeadersFilter.java
@@ -19,7 +19,7 @@ public class SecurityHeadersFilter implements Filter {
             throws IOException, ServletException {
         if (response instanceof HttpServletResponse) {
             HttpServletResponse httpResponse = (HttpServletResponse) response;
-            HttpServletRequest httpRequest = (HttpServletRequest) request;
+     
             
             httpResponse.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' https://code.jquery.com/jquery-3.2.1.js; object-src 'none'; frame-ancestors 'none';font-src 'self' https://fonts.gstatic.com/s/poppins/v21/ https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/webfonts/ ; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css https://fonts.googleapis.com/css2");
             httpResponse.setHeader("X-Frame-Options", "DENY");
